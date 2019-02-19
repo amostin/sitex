04294c31811d20dcf6e2829fcfead179db398388
amostin <amostin@hotmail.fr>
2019-02-19 14:49:10 +0100

    phase_00 coresspond sauf css mais phase_01 n'est pas la meme alors que cest cense letre

---
+++ yes/2_SITEX/phase_00/INC/template.inc.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>SITEX1819.00</title>
+		<link rel="icon" href="favico.ico">
+		<meta name="author" content="Mostin Ambroise">
+		<meta name="version" content="1819-SITEX-00">
+        <meta name="description" content="EPHEC-T2112 - site d'examen 1819">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="IMG/apple-touch-icon.png">
+
+        <link rel="stylesheet" href="CSS/normalize.min.css">
+        <link rel="stylesheet" href="CSS/main.css">
+        <link rel="stylesheet" href="CSS/custom.css">
+
+
+        <script src="/All/mo/modernizr.min.js"></script>
+
+        <script src="/All/jQ/jquery.min.js"></script>
+        <script src="JS/main.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="header-container">
+
+            <header class="wrapper clearfix">
+                <div class="ico">
+                    <img  src="IMG/template.jpg" alt="logo" />
+                </div>
+                <h1 class="title"><?=$param['titleText'] ?></h1>
+                <nav id="menu">
+                    <?=$param['navContent'] ?>
+                </nav>
+            </header>
+        </div>
+
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+
+                <article>
+                    <?=$param['mainContent'] ?>
+                </article>
+
+                <aside>
+                    <?=$param['asideContent'] ?>
+                </aside>
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+        <div class="footer-container">
+            <footer class="wrapper">
+                <section id="infos">
+                    <?=$param['footerSection'] ?>
+
+                </section>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ yes/2_SITEX/phase_00/index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Moi
+ * Date: 09/02/2019
+ * Time: 23:08
+ */
+include("INC/custom.lib.php");
+$param = array(   "titleText"=> "Nom de site en param"
+    ,"mainContent" => "<header>
+                        <h1>Bienvenue</h1>
+                        <p LANG=\"fr\">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </header>
+                    <section hidden>
+                        <h2>article section h2</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                    </section>
+                    <section hidden>
+                        <h2>article section h2</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                    </section>
+                    <footer hidden>
+                        <h3>article footer h3</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+                    </footer>"
+    ,"asideContent"=> "                    <h3>aside</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>"
+    ,"footerSection"=> footerContent()
+    ,"navContent"=> "                    <ul>
+                        <li><a href=\"#\">accueil</a></li>
+                        <li><a href=\"#\">tp</a></li>
+                        <li><a href=\"#\">connexion</a></li>
+                    </ul>"
+
+);
+include("INC/template.inc.php");
+
+
+
+
