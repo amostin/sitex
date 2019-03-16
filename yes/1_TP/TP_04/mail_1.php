d03489662d817e66caa92ee1d532993fc588bf90
amostin <amostin@hotmail.fr>
2019-03-16 11:41:33 +0100

    tp4 mail_1 fini

---
+++ yes/1_TP/TP_04/mail_1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Moi
+ * Date: 16/03/2019
+ * Time: 10:51
+ */
+
+$to = 'amostin@hotmail.fr';
+
+$from = 'From: a.mostin@students.ephec.be';
+
+$subject = 'test tp4';
+
+$message = 'Bonjour,
+
+Voici mon mail « structuré ».
+
+BàT.man';
+
+if (mail($to, $subject, $message, $from)) echo "Sending success !";
