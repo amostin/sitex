841fc150f32cdb0d20c57a223ef98b4411e0302e
amostin <amostin@hotmail.fr>
2019-03-19 17:47:15 +0100

    modif css

--- yes/2_SITEX/phase_01b_test/INC/Request.inc.php
+++ yes/2_SITEX/phase_01b_test/INC/Request.inc.php
@@ -64,7 +64,7 @@ class Request
 //echo "ooo";
 //var_dump($this->iDebug);
 //$this->iDebug->send();
-        //$jeNexistePas++;
+        $jeNexistePas++;
         $phpError = error_get_last();
 //var_dump($phpError);
 
